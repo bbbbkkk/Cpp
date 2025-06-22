27690653f4d3ab70506bba36b41f63b46fa9e8d0
bbbkkk <sbkhao666@gmail.com>
2025-06-22 11:23:20 -0700

    Update test.h

--- 日期使用/test.h
+++ 日期使用/test.h
@@ -5,26 +5,34 @@ using namespace std;
 
 class data
 {
+
 public:
     data(int year = 1900, int month = 1, int day = 1); // 构造函数
     ~data();                                           // 析构函数
-    data(const data &d3)                               // 拷贝析构
-    {
-        cout << "data(const data&d3)" << endl;
-        _year = d3._year;
-        _month = d3._month;
-        _day = d3._day;
-    }
-    void Print();
-    bool operator<(const data &d2);
-    bool operator==(const data &d2);
-    bool operator<=(const data &d2);
-    bool operator>(const data &d2);
-    bool operator>=(const data &d2);
-    bool operator!=(const data &d2);
-    int getday(int month, int year);
+    data(const data &d);                               // 拷贝构造
+    void Print();                                      // 打印
+    // 比较
+    bool operator<(const data &d1);
+    bool operator>(const data &d1);
+    bool operator==(const data &d1);
+    bool operator!=(const data &d1);
+    bool operator>=(const data &d1);
+    bool operator<=(const data &d1);
+    // 天数获取
+    int getday(int year, int month);
+    // 加减
     data &operator+=(int day);
-    data operator+(int day);
+    data operator+(int day); // operator+通过operator+=来实现
+    data operator++();       // 前置++  ++x
+    data operator++(int);    // 后置++  x++
+    data &operator-=(int day);
+    data operator-(int day);
+    // 日期相减d1-d2
+    int operator-(const data &d1);
+    // 自定义类型流插入
+    friend ostream &operator<<(ostream &out, const data &d);
+    // 自定义类型流输出
+    friend istream &operator>>(istream &in, data &d);
 
     // private:
     int _year;
